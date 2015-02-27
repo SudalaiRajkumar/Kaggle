c01e11f49ab9120232176dc2bf06bdf3bebf473a
SudalaiRajkumar <ssraj.rox@gmail.com>
2015-02-27 11:56:14 +0530

    Update and rename Avazu-CTR to Avazu-CTR/finalModel.py

---
+++ Avazu-CTR/finalModel.py
@@ -0,0 +1,317 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 21 18:20:23 2014
+
+Used interactions using this post
+http://www.kaggle.com/c/avazu-ctr-prediction/forums/t/11331/feature-vector-dimension
+"""
+
+from datetime import datetime
+from csv import DictReader
+from math import exp, log, sqrt
+
+
+# TL; DR, the main training process starts on line: 250,
+# you may want to start reading the code from there
+
+
+##############################################################################
+# parameters #################################################################
+##############################################################################
+
+# A, paths
+data_path = "Path to data"
+train = data_path+'train.csv'               # path to training file
+test = data_path+'test.csv'                 # path to testing file
+submission = 'submission.csv'  # path of to be outputted submission file
+
+# B, model
+alpha = .08  # learning rate
+beta = 1.   # smoothing parameter for adaptive learning rate
+L1 = 3.    # L1 regularization, larger value means more regularized
+L2 = 1.     # L2 regularization, larger value means more regularized
+
+# C, feature/hash trick
+D = 2 ** 25             # number of weights to use
+interaction = False     # whether to enable poly2 feature interactions
+
+# D, training/validation
+epoch = 1       # learn training data for N passes
+holdafter = None   # data after date N (exclusive) are used as validation
+holdout = None  # use every N training instance for holdout validation
+
+
+##############################################################################
+# class, function, generator definitions #####################################
+##############################################################################
+
+class ftrl_proximal(object):
+    ''' Our main algorithm: Follow the regularized leader - proximal
+
+        In short,
+        this is an adaptive-learning-rate sparse logistic-regression with
+        efficient L1-L2-regularization
+
+        Reference:
+        http://www.eecs.tufts.edu/~dsculley/papers/ad-click-prediction.pdf
+    '''
+
+    def __init__(self, alpha, beta, L1, L2, D, interaction):
+        # parameters
+        self.alpha = alpha
+        self.beta = beta
+        self.L1 = L1
+        self.L2 = L2
+
+        # feature related parameters
+        self.D = D
+        self.interaction = interaction
+
+        # model
+        # n: squared sum of past gradients
+        # z: weights
+        # w: lazy weights
+        self.n = [0.] * D
+        self.z = [0.] * D
+        self.w = {}
+
+    def _indices(self, x):
+        ''' A helper generator that yields the indices in x
+
+            The purpose of this generator is to make the following
+            code a bit cleaner when doing feature interaction.
+        '''
+
+        # first yield index of the bias term
+        yield 0
+
+        # then yield the normal indices
+        for index in x:
+            yield index
+
+        # now yield interactions (if applicable)
+        if self.interaction:
+            D = self.D
+            L = len(x)
+
+            x = sorted(x)
+            for i in xrange(L):
+                for j in xrange(i+1, L):
+                    # one-hot encode interactions with hash trick
+                    yield abs(hash(str(x[i]) + '_' + str(x[j]))) % D
+
+    def predict(self, x):
+        ''' Get probability estimation on x
+
+            INPUT:
+                x: features
+
+            OUTPUT:
+                probability of p(y = 1 | x; w)
+        '''
+
+        # parameters
+        alpha = self.alpha
+        beta = self.beta
+        L1 = self.L1
+        L2 = self.L2
+
+        # model
+        n = self.n
+        z = self.z
+        w = {}
+
+        # wTx is the inner product of w and x
+        wTx = 0.
+        for i in self._indices(x):
+            sign = -1. if z[i] < 0 else 1.  # get sign of z[i]
+
+            # build w on the fly using z and n, hence the name - lazy weights
+            # we are doing this at prediction instead of update time is because
+            # this allows us for not storing the complete w
+            if sign * z[i] <= L1:
+                # w[i] vanishes due to L1 regularization
+                w[i] = 0.
+            else:
+                # apply prediction time L1, L2 regularization to z and get w
+                w[i] = (sign * L1 - z[i]) / ((beta + sqrt(n[i])) / alpha + L2)
+
+            wTx += w[i]
+
+        # cache the current w for update stage
+        self.w = w
+
+        # bounded sigmoid function, this is the probability estimation
+        return 1. / (1. + exp(-max(min(wTx, 35.), -35.)))
+
+    def update(self, x, p, y):
+        ''' Update model using x, p, y
+
+            INPUT:
+                x: feature, a list of indices
+                p: click probability prediction of our model
+                y: answer
+
+            MODIFIES:
+                self.n: increase by squared gradient
+                self.z: weights
+        '''
+
+        # parameter
+        alpha = self.alpha
+
+        # model
+        n = self.n
+        z = self.z
+        w = self.w
+
+        # gradient under logloss
+        g = p - y
+
+        # update z and n
+        for i in self._indices(x):
+            sigma = (sqrt(n[i] + g * g) - sqrt(n[i])) / alpha
+            z[i] += g - sigma * w[i]
+            n[i] += g * g
+
+
+def logloss(p, y):
+    ''' FUNCTION: Bounded logloss
+
+        INPUT:
+            p: our prediction
+            y: real answer
+
+        OUTPUT:
+            logarithmic loss of p given y
+    '''
+
+    p = max(min(p, 1. - 10e-15), 10e-15)
+    return -log(p) if y == 1. else -log(1. - p)
+
+
+def data(path, D):
+    ''' GENERATOR: Apply hash-trick to the original csv row
+                   and for simplicity, we one-hot-encode everything
+
+        INPUT:
+            path: path to training or testing file
+            D: the max index that we can hash to
+
+        YIELDS:
+            ID: id of the instance, mainly useless
+            x: a list of hashed and one-hot-encoded 'indices'
+               we only need the index since all values are either 0 or 1
+            y: y = 1 if we have a click, else we have y = 0
+    '''
+
+    for t, row in enumerate(DictReader(open(path))):
+        # process id
+        ID = row['id']
+        del row['id']
+
+        # process clicks
+        y = 0.
+        if 'click' in row:
+            if row['click'] == '1':
+                y = 1.
+            del row['click']
+
+        # extract date
+        date = int(row['hour'][4:6])
+
+        # turn hour really into hour, it was originally YYMMDDHH
+        row['hour'] = row['hour'][6:]
+
+	# creating two way feature interactions for some variables based on the feature explanations (not an ideal method though!) 
+	row['C1_bannerpos'] = row['C1']+ "_" + row['banner_pos']
+	row['site_app_category'] = row['site_category'] + '_' +row['app_category']
+	row['site_domin_app_category'] = row['site_domain'] + '_' + row['app_category']
+	row['app_domain_site_category'] = row['app_domain'] + '_' + row['site_category']
+	row['banner_pos_site_id'] = row['banner_pos'] + '_' + row['site_id']
+	row['banner_pos_app_id'] = row['banner_pos'] + '_' + row['app_id']
+	row['banner_pos_device_model'] = row['banner_pos'] + '_' + row['device_model']
+	row['banner_pos_device_conn_type'] = row['banner_pos'] + '_' + row['device_conn_type']
+	row['site_id_device_model'] = row['site_id'] + '_' + row['device_model']
+	row['app_id_device_model'] = row['app_id'] + '_' + row['device_model']
+	row['site_id_app_id'] = row['site_id'] + '_' + row['app_id']
+	row['site_id_device_conn_type'] = row['site_id'] + '_' +row['device_conn_type']
+	row['app_id_device_conn_type'] = row['app_id'] + '_' +row['device_conn_type']
+	row['C14_C17'] = row['C14'] + '_' + row['C17']
+	row['C14_C20'] = row['C14'] + '_' + row['C20']
+	row['C15_C16'] = row['C15'] + '_' + row['C16']
+	row['C15_C18'] = row['C15'] + '_' + row['C18']
+	row['C17_C20'] = row['C17'] + '_' + row['C20']
+	row['C16_C18'] = row['C16'] + '_' + row['C18']
+	row['C19_C21'] = row['C19'] + '_' + row['C21']
+	row['C20_C21'] = row['C20'] + '_' + row['C21']
+	row['C20_site_id'] = row['C20'] + '_' + row['site_id']
+	row['C17_site_id'] = row['C17'] + '_' + row['site_id']
+	row['C20_app_id'] = row['C20'] + '_' + row['app_id']
+	row['C17_app_id'] = row['C17'] + '_' + row['app_id']
+	
+
+        # build x
+        x = []
+        for key in row:
+            value = row[key]
+
+            # one-hot encode everything with hash trick
+            index = abs(hash(key + '_' + value)) % D
+            x.append(index)
+
+        yield t, date, ID, x, y
+
+
+##############################################################################
+# start training #############################################################
+##############################################################################
+
+start = datetime.now()
+
+# initialize ourselves a learner
+learner = ftrl_proximal(alpha, beta, L1, L2, D, interaction)
+
+# start training
+for e in xrange(epoch):
+    loss = 0.
+    count = 0
+
+    for t, date, ID, x, y in data(train, D):  # data is a generator
+        #    t: just a instance counter
+        # date: you know what this is
+        #   ID: id provided in original data
+        #    x: features
+        #    y: label (click)
+
+        # step 1, get prediction from learner
+        p = learner.predict(x)
+
+        if (holdafter and date > holdafter) or (holdout and t % holdout == 0):
+            # step 2-1, calculate validation loss
+            #           we do not train with the validation data so that our
+            #           validation loss is an accurate estimation
+            #
+            # holdafter: train instances from day 1 to day N
+            #            validate with instances from day N + 1 and after
+            #
+            # holdout: validate with every N instance, train with others
+            loss += logloss(p, y)
+            count += 1
+        else:
+            # step 2-2, update learner with label (click) information
+            learner.update(x, p, y)
+
+    #print('Epoch %d finished, validation logloss: %f, elapsed time: %s' % (
+    #    e, loss/count, str(datetime.now() - start)))
+
+
+##############################################################################
+# start testing, and build Kaggle's submission file ##########################
+##############################################################################
+
+with open(submission, 'w') as outfile:
+    outfile.write('id,click\n')
+    for t, date, ID, x, y in data(test, D):
+        p = learner.predict(x)
+        outfile.write('%s,%s\n' % (ID, str(p)))
